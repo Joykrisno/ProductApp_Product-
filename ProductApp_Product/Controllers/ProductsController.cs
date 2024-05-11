e846482925c0b91cf7de4274d16151aa0af8b944
Joy Krisno Ray <joykrisnoray@gmail.com>
2024-05-11 21:33:06 +0600

    Add project files.

---
+++ ProductApp_Product/Controllers/ProductsController.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp_Product.Models;
+using System.Data.SqlClient;
+
+namespace ProductApp_Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly string connectionString;
+
+        public ProductsController(IConfiguration configuration)
+
+        {
+            connectionString = configuration["ConnectionStrings:SqlServerDb"] ?? "";
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductDto productDto)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = @"INSERT INTO products (name, brand, category, price, description)
+                   VALUES (@name, @brand, @category, @price, @description)";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", productDto.Name);
+                        command.Parameters.AddWithValue("@brand", productDto.Brand);
+                        command.Parameters.AddWithValue("@category", productDto.Category);
+                        command.Parameters.AddWithValue("@price", productDto.Price);
+                        command.Parameters.AddWithValue("@description", productDto.Description);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry,But we Have an Exception");
+                return BadRequest(ModelState);
+            }
+
+
+
+            return Ok();
+
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM products";
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Product product = new Product();
+
+                                product.Id = reader.GetInt32(0);
+                                product.Name = reader.GetString(1);
+                                product.Brand = reader.GetString(2);
+                                product.Category = reader.GetString(3);
+                                product.Price = reader.GetDecimal(4);
+                                product.Description = reader.GetString(5);
+                                product.CreatedAt = reader.GetDateTime(6);
+
+                                products.Add(product);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry,But we Have an Exception");
+                return BadRequest(ModelState);
+            }
+
+
+
+            return Ok(products);
+        }
+
+
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductId(int id)
+        {
+            Product product = new Product();
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM products WHERE id=@id";
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                product.Id = reader.GetInt32(0);
+                                product.Name = reader.GetString(1);
+                                product.Brand = reader.GetString(2);
+                                product.Category = reader.GetString(3);
+                                product.Price = reader.GetDecimal(4);
+                                product.Description = reader.GetString(5);
+                                product.CreatedAt = reader.GetDateTime(6);
+                            }
+                            else
+                            {
+                                return NotFound();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry, but we encountered an exception");
+                return BadRequest(ModelState);
+            }
+
+            return Ok(product);
+        }
+
+
+
+        [HttpPut]
+        public IActionResult UpdateProduct(int id,ProductDto productDto)
+        {
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = @"UPDATE products SET name = @name,brand = @brand,  category = @category, price = @price, description = @description WHERE id = @id";
+
+                    using (var command = new SqlCommand(sql,connection))
+                    {
+                        command.Parameters.AddWithValue("@name", productDto.Name);
+                        command.Parameters.AddWithValue("@brand", productDto.Brand);
+                        command.Parameters.AddWithValue("@category", productDto.Category);
+                        command.Parameters.AddWithValue("@price", productDto.Price);
+                        command.Parameters.AddWithValue("@description", productDto.Description);
+                        command.Parameters.AddWithValue("@id", id);
+                        command.ExecuteNonQuery();
+                    }
+
+                }
+            }
+            catch
+            {
+
+            }
+
+            return Ok();
+        }
+
+
+
+    }
+}
+
---
+++ ProductApp_Product/Models/ProductDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApp_Product.Models
+{
+    public class ProductDto
+    {
+        [Required]
+        public string Name { get; set; } = "";
+
+        [Required]
+        public string Brand { get; set; } = "";
+
+        [Required]
+        public string Category { get; set; } = "";
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Description { get; set; } = "";
+
+
+    }
+}
